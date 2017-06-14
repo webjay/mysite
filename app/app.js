1c06c9782681f04dcdf30045f34d4454e5cc6155
Jacob Friis Saxberg <jacob@webcom.dk>
2017-06-14 23:33:01 +0200

    v2

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 /* eslint-env browser */
 'use strict';
 
-var tagstrip_re = /(\.|\?|!)(\s#\w+)+$/m;
 var url = 'https://d2nb7t6y3zkbd1.cloudfront.net/instagrams.json';
 
 function getPhotos (url, handler) {
@@ -16,43 +15,47 @@ function getPhotos (url, handler) {
 }
 
 function insertPhotos (photos) {
-  if (photos.length === 0) return;
-  var columnCount = 3;
-  var rowsCount = Math.floor(photos.length / columnCount);
-  if (rowsCount === 0) return;
   var grid = document.getElementById('photos');
-  var firstrow = grid.getElementsByClassName('row')[0];
+  var card = grid.getElementsByClassName('imgbox')[0];
   var fragment = document.createDocumentFragment();
-  var imgNo = 0;
-  for (var rowNo = 0; rowNo < rowsCount; rowNo++) {
-    var row = (rowNo !== 0) ? firstrow.cloneNode(true) : firstrow;
-    var columns = row.getElementsByClassName('card');
-    for (var colNo = 0; colNo < columnCount; colNo++) {
-      var anchor = columns[colNo].getElementsByTagName('a')[0];
-      var image = columns[colNo].getElementsByTagName('img')[0];
-      var caption = columns[colNo].getElementsByClassName('card-text')[0];
-      anchor.attributes.href.value = photos[imgNo].link;
-      image.attributes.src.value = photos[imgNo].images.low_resolution.url;
-      image.attributes.alt.value = photos[imgNo].tags.join();
-      if (photos[imgNo].caption !== null) {
-        caption.textContent = photos[imgNo].caption.text.replace(tagstrip_re, '$1');
-      } else {
-        caption.textContent = '';
-      }
-      imgNo++;
-    }
-    if (rowNo !== 0) {
-      fragment.appendChild(row);
-    }
+  for (var i = 0; i < photos.length; i++) {
+    var clone = (i === 0) ? card : card.cloneNode(true);
+    var anchor = clone.getElementsByTagName('a')[0];
+    var image = clone.getElementsByTagName('img')[0];
+    anchor.setAttribute('href', photos[i].link);
+    image.setAttribute('src', photos[i].images.low_resolution.url);
+    image.setAttribute('alt', photos[i].tags.join());
+    image.onclick = clickHandlerOn.bind(null, photos[i]);
+    fragment.appendChild(clone);
   }
   grid.appendChild(fragment);
 }
 
-function init () {
-  getPhotos(url, insertPhotos);
+function clickHandlerOn (photo, event) {
+  event.preventDefault();
+  var img = event.target;
+  // img.parentNode.parentNode.setAttribute('class', 'imgbox-big');
+  img.setAttribute('src', photo.images.standard_resolution.url);
+  img.setAttribute('class', 'img-big');
+  img.onclick = clickHandlerOff.bind(null, photo);
 }
 
-document.addEventListener('DOMContentLoaded', init);
-if (document.readyState !== 'loading') {
-  init();
+function clickHandlerOff (photo, event) {
+  event.preventDefault();
+  var img = event.target;
+  // img.parentNode.parentNode.setAttribute('class', 'imgbox');
+  img.setAttribute('class', 'img-std');
+  img.onclick = clickHandlerOn.bind(null, photo);
 }
+
+function init () {
+  getPhotos(url, function (photos) {
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', insertPhotos.bind(null, photos));
+    } else {
+      insertPhotos(photos);
+    }
+  });
+}
+
+init();
